071ac9a0adc809a75b9b54fb45fe96130a9bcbc9
venu bhat <venubha147@gmail.com>
2026-09-04 21:59:20 +0530

    Test

--- app/controllers/affiliate/wallet_controller.rb
+++ app/controllers/affiliate/wallet_controller.rb
@@ -1,7 +1,7 @@
 class Affiliate::WalletController < Affiliate::ApplicationController
   def show
     @wallet = current_affiliate.affiliate_wallet || current_affiliate.create_affiliate_wallet!(balance: 0)
-    @transactions = @wallet.affiliate_wallet_transactions.recent.page(params[:page]).per(20)
+    @transactions = @wallet.affiliate_wallet_transactions.recent.includes(:booking).page(params[:page]).per(20)
     @referred_customers_count = current_affiliate.referred_customers.count
   end
 end
